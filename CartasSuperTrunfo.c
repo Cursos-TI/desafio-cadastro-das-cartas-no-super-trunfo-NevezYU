2e28474512c5c32320ab09d375211b8ca8a77920
NevezYU <gabrieldaadrenalina@gmail.com>
2025-07-22 20:29:45 +0000

    terminado jogo das CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main () {
     printf("Desafio das Cartas!\n");
     
-    char estado1, estado2;
+    char estado1[20], estado2[25];
     char codigo1[5], codigo2[5];
     char cidade1[50], cidade2[50];
     int populacao1, populacao2;
@@ -12,8 +12,8 @@ int main () {
     int pontos1, pontos2;
 
     printf("    Carta 1   \n");
-    printf("Estado: ");
-    scanf(" %c", &estado1);
+    printf("Estado:");
+    scanf(" %s", &estado1);
 
     printf("Código (ex: A01): ");
     scanf("%s", codigo1);
@@ -36,8 +36,8 @@ int main () {
     printf("\n");
 
     printf("   Carta 2   \n");
-    printf("Estado: ");
-    scanf(" %c", &estado2);
+    printf("Estado:");
+    scanf(" %s", &estado2);
 
     printf("Código (ex: B03): ");
     scanf("%s", codigo2);
@@ -57,9 +57,25 @@ int main () {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontos2);
 
-    printf("\n");
+    printf("   Informações das Cartas   \n");
 
+    printf("Carta: 1\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f km\n", area1);
+    printf("PIB: %f bilhões de reais\n", pib1);
+    printf("Número de pontos turísticos: %d\n", pontos1);
 
+    printf("Carta: 2\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f km\n", area2);
+    printf("PIB: %f bilhões de reais\n", pib2);
+    printf("Número de pontos turísticos: %d\n", pontos2);
 
-
+    return 0;
 }
